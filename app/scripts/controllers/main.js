6422756ab9506e2467606dcdeec01d4d289a9c48
Brennan Karrer <brennan.karrer@broadbean.com>
2016-06-03 15:48:50 -0700

    CS-3: Add some styling and sorting capabilities to the list of deals

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,53 +9,32 @@
  */
 angular.module('CheapSharkAPP')
   .controller('MainCtrl', ['$scope', 'FetchDataFactory', function ($scope, FetchDataFactory) {
+
     $scope.FetchDataFactory = FetchDataFactory;
-    
+
     $scope.deals_list = [];
-    
     $scope.range_numbers = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
+    $scope.sortType = 'title';
+
+    /* Invoking the function fetchData from the service FetchDataFactory
+       .then() waits for the data to properly be returned prior to executing any
+       functionality to be performed on the data */
     $scope.FetchDataFactory.fetchData().then(function successCallback(response) {
-        
+
         /* Response data has come back successfully, proceed with calculations */
         if (response.status === 200 && response.statusText === 'OK') {
-          
           for (var i = 0; i < response.data.length; i++) {
-            // Parse the metacriric score to ensure it is an integer
+            /* Parse the metacriric score to ensure it is an integer */
             response.data[i].metacriticScore = parseInt(response.data[i].metacriticScore);
             response.data[i].score = Math.round(response.data[i].metacriticScore / 10) * 10;
-            
-            // Push all of the deals objects to the $scope.deals_list array
+
+            /* Push all of the deals objects to the $scope.deals_list array */
             $scope.deals_list.push(response.data[i]);
-            
-            //console.log(response.data[i]);
           }
-          
         } else {
           console.log("Error", response.status);
         }
-        
-    });
-  }]).filter('rangeFilter', function() {
-    return function(items, range_min, range_max) {
 
-        var filtered = [];
-        var min = parseInt(range_min);
-        var max = parseInt(range_max);
-        // If time is with the range
-        angular.forEach(items, function(item) {
-          console.log(item);
-          console.log(item.score);
-          console.log(min);
-          console.log(max);
-            if( item.score >= min && item.score <= max ) {
-                filtered.push(item);
-            }
-        });
-        return filtered;
-    };
-}).filter('reverse', function() {
-  return function(items) {
-    return items.slice().reverse();
-  };
-});
+    });
+  }]);
